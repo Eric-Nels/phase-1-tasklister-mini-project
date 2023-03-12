95ca002c1c4d5557c4cbecd864f24488c45c3207
Eric Nelson <ernelson12o0@gmail.com>
2023-03-12 17:09:18 -0400

    Lab Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.new_task_description.value)
+  }) 
+
 });
+
+function handleToDo(toDo) {
+  let p = document.createElement('p')
+  p.textContent = toDo
+  document.querySelector('#list').appendChild(p)
+}
